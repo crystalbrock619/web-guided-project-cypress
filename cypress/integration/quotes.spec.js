68c9203a2b9a1abc58a82f3f636f9321ae7d14e2
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-09-17 18:23:51 +0200

    wip

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -6,7 +6,11 @@ describe('Quotes app', () => {
     cy.visit('http://localhost:1234')
   })
 
+  // "it" is a test
   it('sanity check to make sure tests work', () => {
+    // "expect" is an assertion
+    // there can be several assertions per test
     expect(1 + 2).to.equal(3)
+    expect(2 + 2).to.equal(5)
   })
 })
