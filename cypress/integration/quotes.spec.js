a00d48d26f6efd0abe7ef6e8824baab2cf36d520
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-09-17 18:45:22 +0200

    wip

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -21,6 +21,7 @@ describe('Quotes app', () => {
     cy.get('#submitBtn').should('exist')
     cy.get('#cancelBtn').should('exist')
 
-    cy.contains('Submit quote')
+    cy.contains('Submit Quote')
+    cy.contains(/submit quote/i)
   })
 })
