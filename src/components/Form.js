ac32ad21d874c02a1caa6382f95e6823231c08cc
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-09-17 19:29:54 +0200

    wip

--- src/components/Form.js
+++ src/components/Form.js
@@ -56,7 +56,7 @@ export default function Form(props) {
       <button id='submitBtn' disabled={isDisabled()}>
         Submit {values.id ? 'Changes' : 'Quote'}
       </button>
-      <button id='cancelBtn'>Cancel</button>
+      <button id='cancelBtn' onClick={Function.prototype}>Cancel</button>
     </form>
   )
 }
